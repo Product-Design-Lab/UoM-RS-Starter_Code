a6acfaedf23be4620e2ed3931d03e1cc8a3bf919
Nathan <nathan.batham@gmail.com>
2024-02-26 17:48:47 +1100

    Update motor ID change tool

--- Arduino_Firmware/UoM-RS-Change_Motor_ID/UoM-RS-Change_Motor_ID.ino
+++ Arduino_Firmware/UoM-RS-Change_Motor_ID/UoM-RS-Change_Motor_ID.ino
@@ -1,7 +1,23 @@
 
 // MOTOR ID CHANGE TOOL
 
+/*
+UoM-RS-Change_Motor_ID.ino
+--------------------------
+Licenting Information: You are free to use or extend this project
+for educational purposes provided that (1) you do not distribute or
+publish solutions, (2) you retain this notice, and (3) you provide 
+clear attribution to the University of Melbourne, Department of 
+Mechanical Engineering and Product Design Lab.
 
+Attribution Information: The ChessBot project was developed in collaboration
+between Product Design Lab & the University of Melbourne. The core project was 
+primarily developed by Professor Denny Oetomo (doetomo@unimelb.edu.au). All 
+Starter Code and libraries were developed by Nathan Batham of Product
+Design Lab
+(nathan@productdesignlab.com.au)
+
+*/
 
 // Library Inclusions
 #include <SMS_STS.h>
